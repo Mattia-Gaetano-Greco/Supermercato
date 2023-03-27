566e023b264df5f1a07050459d3e3bb8d2d8ff9a
gherardiD <101709520+gherardiD@users.noreply.github.com>
2023-03-27 12:51:49 +0200

    Add files via upload

---
+++ supermercato/src/Prodotto.java
@@ -0,0 +1,11 @@
+public class Prodotto {
+    public int price;
+    public String name;
+
+
+    public Prodotto(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+    
+}
---
+++ supermercato/src/Reparto.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class Reparto {
+    private HashMap <Prodotto, Integer> prodotti;
+
+    public Reparto(){
+        prodotti=new HashMap<Prodotto, Integer>();
+    }
+}
