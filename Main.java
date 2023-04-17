3199d2492c45844a1b73641418813e1d28b9e78a
Mattia-Gaetano-Greco <101709398+Mattia-Gaetano-Greco@users.noreply.github.com>
2023-04-17 19:31:40 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        //Cliente daniele = new Cliente();
-        //Cassa cassa = new Cassa();
-        //cassa.incassa(daniele);
-        Menu.init();
-    }
-}
