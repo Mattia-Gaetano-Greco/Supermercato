18f8183da7a327a5511cf717cb11b0aa948e2c30
Mattia-Gaetano-Greco <101709398+Mattia-Gaetano-Greco@users.noreply.github.com>
2023-05-08 12:47:03 +0200

    Add files via upload

--- Cliente.java
+++ Cliente.java
@@ -1,11 +1,17 @@
 import java.util.HashMap;
-
+/* 
+* Simulatore di cliente
+* @author Mattia Gaetano Greco & Daniele Gherardi
+* @version 1.0.0
+*/
 public class Cliente extends Thread{
     //boolean indica se il prodotto è stato trovato o meno
     private HashMap<Prodotto, Boolean> listaSpesa;
     private static int numero = 0;
     public String nome;
-    
+    /**
+     * Costruttore di default
+     */
     public Cliente(){
         listaSpesa=new HashMap<Prodotto, Boolean>();
         for (int i = 0; i < (int)(Math.random()*3)+1; i++) {
@@ -15,11 +21,14 @@ public class Cliente extends Thread{
         nome = "" + numero;
         numero+=1;
     }
-
+    /**
+     * Fa partire il thread del cliente. La vita del cliente si divide in due fasi: fase di ricerca e fase di pagamento.
+     * Nella fase di ricerca il cliente, ogni 500 millisecondi, prende un prodotto casuale da un reparto casuale impegnandone il semaforo.
+     * Nella fase di pagamento il cliente cerca la cassa con la coda minore e si mette in coda
+     */
     @Override
     public void run() {
-        //gira per il negozio cercando i prodotti
-        //richiama il metodo compra prodotto
+        // prende i prodotti nella sua lista
         for(Prodotto p : listaSpesa.keySet()){
             compraProdotto(p);
             try {
@@ -29,7 +38,8 @@ public class Cliente extends Thread{
             }
         }
         
-        int cassa = 0; // MODIFICARE CON LA CASSA CON CODA PIù CORTA
+        // cerca la cassa con la coda più breve e si accoda in quella cassa
+        int cassa = 0;
         int coda = 100;
         for(int i = 0; i < Supermercato.casse.length; i++){
             Cassa c = Supermercato.casse[i];
@@ -47,7 +57,10 @@ public class Cliente extends Thread{
         }
         Supermercato.casse[cassa].paga(this);
     }
-
+    /** 
+     * Prende il prodotto dal reparto (se lo trova)
+     * @param p Il prodotto che il cliente cerca
+    */
     public void compraProdotto(Prodotto p){
         //se trova il prodotto modifica il value
         //sua listaSpesa
@@ -58,7 +71,10 @@ public class Cliente extends Thread{
             System.out.println("Cliente "+nome+" non ha trovato "+p.name);
         }
     }
-
+    /**
+     * Restituisce la lista della spesa
+     * @return Lista della spesa
+     */
     public HashMap<Prodotto, Boolean> getListaSpesa(){
         return listaSpesa;
     }
--- Prodotto.java
+++ Prodotto.java
@@ -1,14 +1,27 @@
+/**
+ * Unità di prodotto del supermercato
+ * @author Daniele Gherardi
+ * @version 1.0.0
+ */
 public class Prodotto {
     public int price;
     public String name;
     public Reparto reparto;
-
+    /**
+     * Costruttore che crea un prodotto
+     * @param name Nome del prodotto
+     * @param price Prezzo del prodotto
+     * @param reparto Reparto di appartenenza del prodotto
+     */
     public Prodotto(String name, int price, Reparto reparto){
         this.name = name;
         this.price = price;
         this.reparto = reparto;
     }
-    
+    /**
+     * Restituisce la quantità del prodotto nel reparto 
+     * @return quantità di prodotto nel reparto
+     */
     public boolean compra(){
         return reparto.prendi(this);
     }
